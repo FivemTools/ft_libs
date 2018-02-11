18d86b3325e2091c4d65cb7fb10d94b4a637d2e9
samuelds <samuelds.pro@gmail.com>
2018-02-11 14:17:09 +0100

    Add ui, load and debug file

--- __resource.lua
+++ __resource.lua
@@ -8,15 +8,28 @@ dependencies {}
 
 client_scripts {
 
+  -- Utils
+  "src/utils/utils.common.lua",
+  "src/utils/utils.client.lua",
+
   -- Ui
   "src/ui/ui.client.lua",
 
-  -- Run function in frame
+  -- Frames
   "src/frame.client.lua",
+
+  -- Event 100% load
+  "src/client.load.lua",
 
 }
 
 exports {
+
+  -- Utils
+  "TableLength",
+  "Round",
+  "CommaValue",
+  "GetEntityDirection",
 
   -- Ui
   "HelpPromt",
@@ -24,9 +37,26 @@ exports {
   "Message",
   "Notification",
   "Text",
+  "OpenTextInput",
+  "TextNotification",
 
 }
 
-server_scripts {}
+server_scripts {
 
-server_exports {}
+  -- Utils
+  "src/utils/utils.common.lua",
+  "src/utils/utils.server.lua",
+
+  -- Debug
+  "src/debug/debug.server.lua",
+
+}
+
+server_exports {
+
+  -- Utils
+  "TablePrint",
+  "Round",
+
+}

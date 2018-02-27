c6ae95531c8dd58e8335b54e8f8c5c8b25cafea4
samuelds <samuelds.pro@gmail.com>
2018-02-27 22:08:40 +0100

    Add marker manager

---
+++ src/marker/marker.client.lua
@@ -0,0 +1,40 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+--
+
+function Marker(data)
+
+  local self = {}
+  self.type = data.type or 1
+  self.x = data.x or 0.0
+  self.y = data.y or 0.0
+  self.z = data.z or 0.0
+  self.weight = data.weight or 2.0
+  self.height = data.height or 0.5
+  self.red = data.red or 255
+  self.green = data.green or 255
+  self.blue = data.blue or 255
+  self.alpha = data.alpha or 255
+  self.showDistance = data.showDistance or self.weight + 20
+
+  if data.enable ~= nil then
+      self.enable = data.enable
+  else
+      self.enable = true
+  end
+
+  self.Display = function(display)
+    self.display = display
+  end
+
+  self.Show = function()
+    local weight = self.weight * 2.0
+    local height = self.height + 0.0
+    DrawMarker(self.type, self.x, self.y, (self.z - 1), 0, 0, 0, 0, 0, 0, weight, weight, height, self.red, self.green, self.blue, self.alpha, 0, 0, 0, 0)
+  end
+
+  return self
+
+end
---
+++ src/marker/markers.client.lua
@@ -0,0 +1,198 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+--
+
+Markers = {}
+
+function AddMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for name, value in pairs(args[1]) do
+      Citizen.Wait(1)
+      Markers[name] = Marker(value)
+    end
+
+  elseif count == 2 then
+
+    local name = args[1]
+    local value = args[2]
+    Markers[name] = Marker(value)
+
+  else
+
+    return false
+
+  end
+
+end
+
+function RemoveMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for _, name in ipairs(args[1]) do
+      Citizen.Wait(1)
+      if Markers[name] ~= nil then
+        Markers[name] = nil
+      end
+    end
+
+  elseif count == 1 then
+
+    local name = args[1]
+    if Markers[name] ~= nil then
+      Markers[name] = nil
+    end
+
+  else
+
+    return false
+
+  end
+
+end
+
+function EnableMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for name, value in pairs(args[1]) do
+      Citizen.Wait(1)
+      if Markers[name] ~= nil then
+        Markers[name].enable = value
+      end
+    end
+
+  elseif count == 2 then
+
+    local name = args[1]
+    local status = args[2]
+    if Markers[name] ~= nil then
+      Markers[name].enable = status
+    end
+
+  else
+
+    return false
+
+  end
+
+end
+
+function EnableMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for _, name in pairs(args[1]) do
+      Citizen.Wait(1)
+      if Markers[name] ~= nil then
+        Markers[name].enable = true
+      end
+    end
+
+  elseif count == 1 then
+
+    local name = args[1]
+    if Markers[name] ~= nil then
+      Markers[name].enable = true
+    end
+
+  else
+
+    return false
+
+  end
+
+end
+
+function DisableMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for _, name in pairs(args[1]) do
+      Citizen.Wait(1)
+      if Markers[name] ~= nil then
+        Markers[name].enable = false
+      end
+    end
+
+  elseif count == 1 then
+
+    local name = args[1]
+    if Markers[name] ~= nil then
+      Markers[name].enable = false
+    end
+
+  else
+
+    return false
+
+  end
+
+end
+
+function SwitchMarker(...)
+
+  local args = {...}
+  local count = #args
+
+  if count == 1 and type(args[1]) == "table" then
+
+    for name, status in pairs(args[1]) do
+      Citizen.Wait(1)
+      if Markers[name] ~= nil then
+        Markers[name].enable = status
+      end
+    end
+
+  elseif count == 2 then
+
+    local name = args[1]
+    local status = args[2]
+    if Markers[name] ~= nil then
+      Markers[name].enable = status
+    end
+
+  else
+
+    return false
+
+  end
+
+end
+
+function CurrentMarker()
+  return Markers.current
+end
+
+-- Check
+AddRunInFrame(function()
+
+  local playerPed = GetPlayerPed(-1)
+  local playerLocalisation = GetEntityCoords(playerPed)
+
+  for name, marker in pairs(Markers) do
+    if GetDistanceBetweenCoords(marker.x, marker.y, marker.z, playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, true) <= marker.showDistance and marker.enable then
+      marker.Show()
+    end
+  end
+
+end)

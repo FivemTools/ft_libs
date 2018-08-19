0a91b09b37137a8ef0cc62296c09b0951b5444e6
samuelds <samuelds.pro@gmail.com>
2018-08-19 18:31:19 +0200

    fix playerCoords

--- src/entity/entity.client.lua
+++ src/entity/entity.client.lua
@@ -72,7 +72,7 @@ end
 function GetEntityInDirection(range)
 
   local entityWorld = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, range, 0.0)
-  local rayHandle = CastRayPointToPoint(playerPos.x, playerPos.y, playerPos.z, entityWorld.x, entityWorld.y, entityWorld.z, 10, playerPed, 0)
+  local rayHandle = CastRayPointToPoint(playerCoords.x, playerCoords.y, playerCoords.z, entityWorld.x, entityWorld.y, entityWorld.z, 10, playerPed, 0)
   local a, b, c, d, entity = GetRaycastResult(rayHandle)
   if entity ~= nil then
       return entity
